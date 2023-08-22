a9454e362c2296cadc0e527112a581cd663c0c0c
Vadwaaait <gawade.mayuresh03@gmail.com>
2023-08-22 23:53:07 +0530

    view all hotels functionality added in UI

--- src/Pages/UserAllHotels.js
+++ src/Pages/UserAllHotels.js
@@ -3,6 +3,8 @@ import UserNav from '../Components/UserNav'
 import axios from 'axios';
 import HotelShowCard from '../Components/HotelShowCard';
 import { keyboard } from '@testing-library/user-event/dist/keyboard';
+import "../Components/Styles/hotelcardStyle.css"
+
 const UserAllHotels = () => {
 
 
@@ -13,29 +15,43 @@ const UserAllHotels = () => {
 
 
 
-         const fetchdata=()=> axios.get("http://localhost:8080/api/getAllHotels").then(res=>sethotelinfo(res.data))
+        const fetchdata =()=>{
+            fetch("http://localhost:8080/api/getAllHotels").then(resp=>resp.json()).then(resp=> {console.log(resp);
+             sethotelinfo(resp)})
+            
+           
+        }
 
-    useEffect(()=>{
-        fetchdata()
-    },[])
 
+        useEffect(()=>{
+            fetchdata();
+        },[])
+
+    
 
   return (
        <>
 
         <UserNav/>
        
-       {
+        <ul>
+            {
+                hotelinfo.map((item,i)=>{
+                    return (
+                        <div className='hotelcard'>
+                    <li key={i}>
+                        <h2>{item.hotelName}</h2>
+                        <h4>City : {item.hotelCity}</h4>
+                        <h5>Address :{item.hotelAddress}</h5>
+                        <h5>Rooms available : {item.noOfRooms}</h5>
+                        <h4 style={{color:"green"}}>Price : ₹ {item.hotelPrice}</h4>
+                        </li>
+                        </div>
+                    )})
+            }
 
-        hotelinfo.map((data,index)=>{
-            <h1 key={index}>
-                    {data.hotelName
-                    }
-            </h1>
-        })
-
-       }
-        
+        </ul>
+      
 
 
     </>
