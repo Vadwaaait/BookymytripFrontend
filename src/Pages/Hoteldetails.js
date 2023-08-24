8284d15d8ff378757a5f8d8745283c5e4dcf8b1d
Vadwaaait <gawade.mayuresh03@gmail.com>
2023-08-24 15:25:41 +0530

    Added functionality to land on view hotel details page after clicking on view details button from allHotels page and saving the state in cookies and displaying correct info of hotel on view details page

--- src/Pages/Hoteldetails.js
+++ src/Pages/Hoteldetails.js
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import UserNav from '../Components/UserNav'
 import "../Components/Styles/hoteldetails.css"
 import Cookies from 'js-cookie';
+import axios from 'axios';
+
 const Hoteldetails = () => {
 
+
+  const[hotelinfoo,sethotelinfoo]=useState([])
+
+
+
   const hotelid=Cookies.get('hotelidd');
+
+  const fetchdata=()=>{
+
+    fetch("http://localhost:8080/api/user/hotel/id/"+hotelid).then(resp=>resp.json()).then(resp=> {console.log(resp);
+             sethotelinfoo(resp)})
+
+  }
+
+  useEffect(()=>{
+    fetchdata();
+  },[])
+
 
   return (
     <>
      <UserNav /> 
 
         <div className="deschotel">
-            <h1>Hotel Name</h1>
-            <h4>Hotel city</h4>
-            <h3>Hotel Address</h3>
-            <h3>Price</h3>
+            <h1>{hotelinfoo.hotelName}</h1>
+            <h4>{hotelinfoo.hotelCity}</h4>
+            <h3>{hotelinfoo.hotelAddress}</h3>
+            <h3>{hotelinfoo.hotelPrice}</h3>
             <h3>Hotel Description</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ratione ullam corrupti non ea inventore ducimus rerum recusandae iste iure accusamus unde, commodi asperiores, error quasi nulla saepe eum sunt.</p>
             <h5>{hotelid}</h5>
